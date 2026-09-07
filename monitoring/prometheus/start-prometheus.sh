3fcaeb8835e31f293829add4860e107de8b98594
Salman Sayeed <salmansohailuk@gmail.com>
2026-09-07 17:25:20 +0100

    Update start-prometheus.sh

--- monitoring/prometheus/start-prometheus.sh
+++ monitoring/prometheus/start-prometheus.sh
@@ -1,10 +1,1 @@
-#!/bin/sh
 
-set -e
-
-echo "Starting Prometheus..."
-echo "Using configuration: ${CONFIG_FILE}"
-
-exec /bin/prometheus \
-  --config.file="${CONFIG_FILE}" \
-  --storage.tsdb.path=/prometheus
