ce83b47183eb014c7ed0a54309f2408a9e2092d1
Salman Sayeed <salmansohailuk@gmail.com>
2026-09-13 18:45:12 +0100

    Update start.sh

--- monitoring/prometheus/start.sh
+++ monitoring/prometheus/start.sh
@@ -6,6 +6,4 @@ java -jar /cloudwatch_exporter.jar 9106 /etc/cloudwatch-exporter/cloudwatch.yml 
 # run prometheus in foreground (keeps container running)
 exec /bin/prometheus \
   --config.file=/etc/prometheus/prometheus.yml \
-  --storage.tsdb.path=/prometheus \
-  --web.console.libraries=/etc/prometheus/console_libraries \
-  --web.console.templates=/etc/prometheus/consoles
+  --storage.tsdb.path=/prometheus
