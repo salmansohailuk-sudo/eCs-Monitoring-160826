00869829091e7048c5f4d6876acf347e287a5822
Salman Sayeed <salmansohailuk@gmail.com>
2026-09-13 19:46:33 +0100

    Define outputs for EC2 and RDS in output.tf
    Added outputs for EC2 public IP and RDS endpoint.

---
+++ terraform/output.tf
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------
+# Outputs - Helpful values after deployment
+# ---------------------------------------------------------
+
+output "ec2_public_ip" {
+  value = aws_instance.ec2.public_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.address
+}
